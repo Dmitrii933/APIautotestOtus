63f6f80f81b1c9c28278e2268e1af33094ffd33e
Dmitrii933 <dborshik@yandex.ru>
2023-01-09 15:13:12 +0300

    Update rest

---
+++ jenkins/ui-pipeline.groovy
@@ -0,0 +1,28 @@
+timeout(60) {
+    node('maven-slave') {
+        stage('Checkout') {
+            checkout scm
+        }
+        stage('Run tests') {
+            def exitCode = sh(
+                    returnStatus: true,
+                    script: """
+                    mvn test -Dbrowser=$BROWSER -Dbrowser-version=$BROWSER_VERSION -Dwebdriver.base.url=$BASE_URL -Dwebdriver.base.url =$GRID_URL
+                    """
+            )
+            if (exitCode == 1){
+                currentBuild.result = 'UNSTABLE'
+            }
+        }
+        stage('Allure result'){
+            allure([
+                 includeProperties: false,
+                    jdk: '',
+                    properties: [],
+                    reportBuilsPolicy: 'ALWAYS',
+                    
+
+            ])
+        }
+    }
+}
